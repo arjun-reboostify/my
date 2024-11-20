4ea31e084ea3291cc3b5842c8566e147f4125547
arjun-reboostify <reboostify@gmail.com>
2024-11-21 00:04:53 +0530

    d

--- src/components/it/Song.jsx
+++ src/components/it/Song.jsx
@@ -4,7 +4,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 // Import your track and cover images
 import track1 from './music/Espresso.mp3';
 import track2 from './music/Escaping Forever.mp3';
-import track3 from './music/Good Day.mp3';
+import track3 from './music/a.mp3';
 import track4 from './music/Espresso.mp3';
 import coverImage from './music/espresso.jpg';
 import Side from './Sidebar';
